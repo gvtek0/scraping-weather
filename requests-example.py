d0d66255b6a0f59fbc7c1bf039c21731bcc7c7a9
gvtek0 <gvtek0@gmal.com>
2020-05-23 23:12:38 -0700

    soup prettify

--- requests-example.py
+++ requests-example.py
@@ -1,5 +1,10 @@
 #! python3
 import requests
+from bs4 import BeautifulSoup
+
 page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
 print(page.status_code)
 print(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
